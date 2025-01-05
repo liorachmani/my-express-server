459a48aa91b30636b346170d335f3da33a40d59d
Ido Winkler <idowin04@gmail.com>
2025-01-05 15:59:07 +0200

    removed old user register

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { UserController } from "../controllers/user";
 const router = express.Router();
 
-router.post("/", UserController.register);
 router.get("/:id", UserController.getUserById);
 router.put("/:id", UserController.updateUser);
 router.delete("/:id", UserController.deleteUser);
