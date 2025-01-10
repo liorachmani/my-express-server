adacb84bb2b40286837cd5f9ad7336dc8e19beb5
Lior Rachmani <liorachmani@gmail.com>
2025-01-10 12:12:40 +0200

    swagger for post routes

--- routes/postRoutes.ts
+++ routes/postRoutes.ts
@@ -7,10 +7,216 @@ import post, { IPost } from "../models/post";
 const router = express.Router();
 const verifyPostUser = verifyUserOnEntity<IPost>(post);
 
+/**
+ * @swagger
+ * tags:
+ *   name: Posts
+ *   description: The Posts API
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - title
+ *         - content
+ *         - user_id
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The auto-generated id of the post
+ *         title:
+ *           type: string
+ *           description: The title of the post
+ *         content:
+ *           type: string
+ *           description: The content of the post
+ *         user_id:
+ *           type: string
+ *           description: The user id of the post author
+ *       example:
+ *         _id: 67474a3d2651e79673ab702f
+ *         title: My Post
+ *         content: This is the content of the post
+ *         user_id: 6778f7f548cb7802a935f9a4
+ */
+
+/**
+ * @swagger
+ * /post:
+ *   post:
+ *     summary: Create a new post
+ *     description: Create a new post
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: The title of the post
+ *               content:
+ *                 type: string
+ *                 description: The content of the post
+ *             required:
+ *               - title
+ *               - content
+ *     responses:
+ *       201:
+ *         description: Post created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       403:
+ *         description: Forbidden
+ *       500:
+ *         description: Server error
+ */
 router.post("/", authenticate, PostController.createPost);
+
+/**
+ * @swagger
+ * /post:
+ *   get:
+ *     summary: Get all posts
+ *     description: Retrieve a list of all posts
+ *     parameters:
+ *      - name: sender
+ *        in: query
+ *        required: false
+ *        description: A filter to narrow down items by sender.
+ *        schema:
+ *          type: string
+ *     tags:
+ *       - Posts
+ *     responses:
+ *       200:
+ *         description: Returns a list of posts. If no query parameters are provided, returns all items. If query parameters are provided, returns filtered items based on the parameters.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ *       500:
+ *         description: Server error
+ */
 router.get("/", PostController.getPosts);
+
+/**
+ * @swagger
+ * /post/{id}:
+ *   get:
+ *     summary: Get a post by ID
+ *     description: Retrieve a single post by its ID
+ *     tags:
+ *       - Posts
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the post
+ *     responses:
+ *       200:
+ *         description: A single post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       500:
+ *         description: Server error
+ */
 router.get("/:id", PostController.getPostById);
+
+/**
+ * @swagger
+ * /post/{id}:
+ *   put:
+ *     summary: Update a post by ID
+ *     description: Update a post by its ID
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the post
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: The title of the post
+ *               content:
+ *                 type: string
+ *                 description: The content of the post
+ *     responses:
+ *       200:
+ *         description: Post updated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       403:
+ *         description: Forbidden to update the post
+ *       404:
+ *         description: Post not found
+ *       500:
+ *         description: Server error
+ */
 router.put("/:id", authenticate, verifyPostUser, PostController.updatePost);
+
+/**
+ * @swagger
+ * /post/{id}:
+ *   delete:
+ *     summary: Delete a post by ID
+ *     description: Delete a single post by its ID
+ *     tags:
+ *       - Posts
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the post
+ *     responses:
+ *       200:
+ *         description: The deleted post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Post'
+ *       403:
+ *         description: Forbidden to delete the post
+ *       404:
+ *         description: Post not found
+ *       500:
+ *         description: Server error
+ */
 router.delete("/:id", authenticate, verifyPostUser, PostController.deletePost);
 
 export default router;
